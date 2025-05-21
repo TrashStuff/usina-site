be12a919328ec519218fc88afbc1cbfaf3022c19
Breno Calazans <brenocalazans@gmail.com>
2025-05-21 00:09:34 -0300

    Altera description da pagina nos

--- app/nos/page.tsx
+++ app/nos/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import { NavBar } from "../NavBar";
 import { Metadata } from "next";
 
+const nosDescription =
+  "Uma dupla afiada com 15 anos de experiência conjunta, amplificada por uma rede de técnicos e criativos, atuante em diversas áreas: comunicação, arte, e entretenimento, de equipes pequenas à grandes corporações.";
+
 export const metadata: Metadata = {
   title: "Nós | Usina",
-  description: "Sobre nós",
+  description: nosDescription,
   openGraph: {
     title: "Nós | Usina",
-    description: "Sobre nós",
+    description: nosDescription,
     images: ["/nos.jpg"],
   },
 };
@@ -30,10 +33,7 @@ export default function Nos() {
       <NavBar currentPage="nos" />
       <div className="pb-50 z-10 w-full px-5">
         <p className="text-slate-100 font-paragraph uppercase text-xl leading-9 md:text-6xl md:leading-18 max-w-4xl text-pretty z-10 bg-blend-multiply text-shadow-lg">
-          Uma dupla afiada com 15 anos de experiência conjunta, amplificada por
-          uma rede de técnicos e criativos, atuante em diversas áreas:
-          comunicação, arte, e entretenimento, de equipes pequenas à grandes
-          corporações.
+          {nosDescription}
         </p>
       </div>
     </div>
