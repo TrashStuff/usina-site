89e431474ff5e5ccdbe0de35c70bbdc08e6f57fd
Breno Calazans <brenocalazans@gmail.com>
2025-08-04 22:31:50 -0300

    Corrige pagina de projetos

--- app/projetos/page.tsx
+++ app/projetos/page.tsx
@@ -19,17 +19,17 @@ export default async function Projetos() {
     <div className="flex flex-col items-center min-h-screen">
       <NavBar currentPage="projetos" />
 
-      <div className="flex flex-wrap w-full gap-y-10 md:gap-y-0">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 w-full gap-y-10 md:gap-y-0">
         {content.map(({ metadata }) => (
           <Link
             href={`/projetos/${metadata.slug}`}
             key={metadata.slug}
-            className="grow md:aspect-[3/4] relative group md:bg-white md:flex flex-col justify-between md:p-4 text-slate-900 min-w-[300px]"
+            className="grow md:aspect-[3/4] relative group md:bg-white md:flex flex-col justify-between md:p-4 text-slate-900 md:min-w-[300px]"
           >
             <img
               src={metadata.image}
               alt={metadata.title}
-              className="md:absolute max-h-80 md:max-h-full top-0 left-0 h-full w-full object-cover pointer-events-none saturate-20 z-10 group-hover:opacity-0 transition-opacity"
+              className="md:absolute max-h-80 aspect-[3/4] md:max-h-full top-0 left-0 h-full w-full object-cover pointer-events-none saturate-20 z-10 group-hover:opacity-0 transition-opacity"
             />
             <h3 className="text-slate-100 md:text-black p-4 md:p-0 text-4xl font-normal">
               {metadata.title}
@@ -37,7 +37,9 @@ export default async function Projetos() {
             <div className="text-slate-100 md:text-black text-4xl md:text-lg p-4 md:p-0 font-light flex flex-col justify-around gap-4">
               <div>
                 <div className="text-sm">Atividade</div>
-                <div>{metadata.tags.join(", ")}</div>
+                <div className="whitespace-break-spaces">
+                  {metadata.tags.join(", ")}
+                </div>
               </div>
               <div>
                 <div className="text-sm">Cliente</div>
