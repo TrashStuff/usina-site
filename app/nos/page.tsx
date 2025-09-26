be465eb039f243f23b6e343c913b8481dd943cb4
Miguelito Mermelstein <trashstuff85@gmail.com>
2025-09-25 21:41:23 -0300

    Update page.tsx

--- app/nos/page.tsx
+++ app/nos/page.tsx
@@ -1,18 +1,19 @@
 import { NavBar } from "../NavBar";
 import { Metadata } from "next";
 
-const nosDescription =
-"Usina é uma produtora formada pela dupla Hugo Rocha e Miguel Mermelstein.\n\n" +
-  "Criamos soluções sonoras para projetos audiovisuais.\n\n" +
-  "Produzimos trilhas sonoras e cuidamos de todas as etapas de finalização de áudio para filmes, podcasts, publicidade, instalações e teatro.";
+const nosDescription = [
+  "Usina é uma produtora formada pela dupla Hugo Rocha e Miguel Mermelstein.",
+  "Criamos soluções sonoras para projetos audiovisuais.",
+  "Produzimos trilhas sonoras e cuidamos de todas as etapas de finalização de áudio para filmes, podcasts, publicidade, instalações e teatro."
+];
 
 export const metadata: Metadata = {
   title: "Nós | Usina",
-  description: nosDescription,
+  description: nosDescription.join(" "),
   metadataBase: new URL(process.env.NEXT_PUBLIC_URL ?? ""),
   openGraph: {
     title: "Nós | Usina",
-    description: nosDescription,
+    description: nosDescription.join(" "),
     images: ["/nos.jpg"],
   },
 };
@@ -25,9 +26,13 @@ export default function Nos() {
       <NavBar currentPage="nos" />
 
       <div className="pb-50 z-10 w-full px-5">
-        <p className="text-justify text-slate-100 bg-black bg-opacity-80 p-4 rounded font-sans text-base leading-7 md:text-2xl md:leading-9 max-w-4xl text-pretty z-10 text-shadow-lg">
-          {nosDescription}
-        </p>
+        <div className="ml-10 md:ml-20 lg:ml-40 text-right text-slate-100 bg-black bg-opacity-80 p-4 rounded font-sans text-base leading-7 md:text-2xl md:leading-9 max-w-4xl text-pretty z-10 text-shadow-lg">
+          {nosDescription.map((p, i) => (
+            <p key={i} className="mb-4">
+              {p}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
