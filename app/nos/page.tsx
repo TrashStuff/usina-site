dab08e99bf6f87b55efda12010dc69a293217048
git-hugo-rocha <hugo.gui.rocha@gmail.com>
2025-10-27 13:37:37 -0300

    Update page.tsx

--- app/nos/page.tsx
+++ app/nos/page.tsx
@@ -28,7 +28,7 @@ export default function Nos() {
       <div className="container flex items-center justify-between pb-50 z-10 w-full">
         <div className="text-justify text-slate-100 bg-black bg-opacity-80 p-4 rounded font-sans text-base leading-7 md:text-2xl md:leading-9 max-w-4xl text-pretty z-10 text-shadow-lg flex flex-col gap-3">
           <p>
-            Usina é uma produtora formada pela dupla Hugo Rocha e Miguel
+            Usina é uma produtora formada pela dupla <a href=\"https://cargocollective.com/hugorocha\" target=\"_blank\">Hugo Rocha</a> e Miguel
             Mermelstein.
           </p>
           <p>Criamos soluções sonoras para projetos audiovisuais.</p>
@@ -36,6 +36,9 @@ export default function Nos() {
             Produzimos trilhas sonoras e cuidamos de todas as etapas de
             finalização de áudio para filmes, podcasts, publicidade, instalações
             e teatro.
+          </p>
+          <p>
+            Hugo é formado em Rádio/TV na ECO/UFRJ e é um profissional com mais de 12 anos de experiência em som e audiovisual, com atuação em música, cinema, publicidade, podcasts e artes cênicas. Envolvido em dezenas de projetos em desenho de som, edição de áudio, produção musical, trilha sonora e finalização, seu trabalho combina precisão técnica com uma abordagem artística sensível. Atua também como coordenador de pós-produção, liderando equipes e supervisionando fluxos de trabalho, cronogramas e entregas para múltiplas plataformas. Atuou por 5 anos na Comunicação da Globo, fazendo a gestão de conteúdos audiovisuais para a marca, acompanhando projetos do roteiro à finalização. Teve uma trajetória como editor de vídeo e videomaker e foi fundador da Companhia Volante, realizando projetos autorais coletivos em performance e artes cênicas.
           </p>
         </div>
       </div>
